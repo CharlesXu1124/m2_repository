ff484e7d4abd5b1cba9573311a9a6df5970a6e30
eric8 <eric8@gatech.edu>
2017-09-07 11:48:41 -0400

    added the gradle files -Eric

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,7 +43,7 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 		String str = "";
-		for (int x = input.length() - 1; X >= 0; x--) {
+		for (int x = input.length() - 1; x >= 0; x--) {
 			str += input.charAt(x);
 		}
 		return str;
