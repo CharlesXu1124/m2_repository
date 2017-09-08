d9ebb862c0d5bb0a16f8e9cbea2981e9996e0918
CharlesXu1124 <31540412+CharlesXu1124@users.noreply.github.com>
2017-09-07 23:48:12 -0400

    Edit Person5.java
    added toString method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -1,49 +1,62 @@
 package edu.gatech.oad.antlab.person;
 
 /**
- *  A simple class for person 5
+ *  A simple class for person 4
  *  returns their name and a
  *  modified string 
  *  
  *  @author Bob
- *  @version 1.1
+ *  @version 1.2
  */
 public class Person5 {
   /** Holds the persons real name */
   private String name;
-  	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
   public Person5(String pname) {
     name = pname;
   }
-  	/**
-	 * This method should take the string
-	 * input and return its characters rotated
-	 * 2 positions.
-	 * given "gtg123b" it should return
-	 * "g123bgt".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
-	}
-	
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+    /**
+     * This method should return a string
+     * where each character is 1 greater 
+     * than its previous value.  So
+     * given "abc123" it should return
+     * "bcd234".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+      //Person 4 put your implementation here
+      String a;
+      int len;
+      a = "";
+      len = input.length();
+      for (int i = 0; i < len; i++)
+      {
+        char c = input.charAt(i);
+        int ascii = (int)c;
+        int index = ascii + 1;
+        char b = (char)index;
+        a = a + b;
+      }
+      return a;
+    }
+    
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+    *
+    * @param input the varying string
+    * @return the string representing the 
+    *         object
+    */
+    public String toString(String input) {
+      return name + calc(input);
+    }
 
 }
+
